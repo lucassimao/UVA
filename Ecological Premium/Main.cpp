763bbc0ff1f88e8319a1e70964344396965eba79
Lucas Simao <lsimaocosta@gmail.com>
2009-11-10 18:53:14 -0300

    Problema Ecological Premium resolvido

---
+++ Ecological Premium/Main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::cout;
+using std::endl;
+using std::cin;
+
+int main(int argc,char* argv[]){
+
+	int casos_de_teste=0;
+	cin >> casos_de_teste;
+
+	while(casos_de_teste--){
+		int qtdeFarmers = 0;
+		cin >> qtdeFarmers;
+		double budget = 0.0;
+
+		while(qtdeFarmers--){
+			double farmyard=0.0; 
+			int qtdeAnimals=0,qualidade =0;
+
+			cin >> farmyard;
+			cin >> qtdeAnimals;
+			cin >> qualidade;
+
+			budget += (farmyard/qtdeAnimals)*qualidade*qtdeAnimals;
+		}
+		cout << budget << endl;
+
+	}
+
+
+
+	return EXIT_SUCCESS;
+}
---
+++ Machined Surfaces/Main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::cout;
+using std::endl;
+using std::cin;
+
+#define COLUMNS 25
+
+int main(int argc,char* argv[]){
+
+	return EXIT_SUCCESS;
+}
