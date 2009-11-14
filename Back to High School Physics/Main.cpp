19a3cc15494852950e29132350b779cf084e73f4
Lucas Simao <lsimaocosta@gmail.com>
2009-11-14 17:34:00 -0300

    Questao Back to High School Physics resolvida

---
+++ Back to High School Physics/Main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using std::cin;
+using namespace std;
+
+int main(int argc,char **argv){
+	
+	int v=0,t=0;
+
+	while(!cin.eof()){
+		cin>>v;
+		cin>>t;
+		//double aceleracao = v/t; v = v0+at .:. a = v/t
+		// displacement = s = s0 + v0t + a*pow(t,2)/2
+		cout << (v*t*2) << endl;
+	}
+
+	return 0;
+}
