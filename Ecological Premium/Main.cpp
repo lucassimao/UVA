99c14b2609c9b9c1c72f1c29a76512d2117a7eb0
Lucas Simao <lsimaocosta@gmail.com>
2009-11-14 17:13:52 -0300

    Algumas alteracoes minimas

--- Ecological Premium/Main.cpp
+++ Ecological Premium/Main.cpp
@@ -1,30 +1,41 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 
 using std::string;
 using std::cout;
 using std::endl;
 using std::cin;
+using std::istream;
+using std::ifstream;
+using std::ios;
+
+#define DEVELOPMENT_ENV
 
 int main(int argc,char* argv[]){
+#ifdef DEVELOPMENT_ENV
+	ifstream *input = new ifstream("input.txt",ios::in);
+#else
+	istream *input = &cin;
+#endif
 
 	int casos_de_teste=0;
-	cin >> casos_de_teste;
+	*input >> casos_de_teste;
 
 	while(casos_de_teste--){
 		int qtdeFarmers = 0;
-		cin >> qtdeFarmers;
+		*input >> qtdeFarmers;
 		double budget = 0.0;
 
 		while(qtdeFarmers--){
 			double farmyard=0.0; 
 			int qtdeAnimals=0,qualidade =0;
 
-			cin >> farmyard;
-			cin >> qtdeAnimals;
-			cin >> qualidade;
+			*input >> farmyard;
+			*input >> qtdeAnimals;
+			*input >> qualidade;
 
-			budget += (farmyard/qtdeAnimals)*qualidade*qtdeAnimals;
+			budget += farmyard*qualidade;
 		}
 		cout << budget << endl;
 
