183ccafe4ec317acd07a4025ec8115c5bc32d59e
Lucas Simao <lsimaocosta@gmail.com>
2009-11-15 11:15:12 -0300

    Tentativa de correcao do problema Back to High School Physics, trocando cin e cout por scanf e printf, respectivamente

--- Back to High School Physics/Main.cpp
+++ Back to High School Physics/Main.cpp
@@ -7,12 +7,8 @@ int main(int argc,char **argv){
 	
 	int v=0,t=0;
 
-	while(!cin.eof()){
-		cin>>v;
-		cin>>t;
-		//double aceleracao = v/t; v = v0+at .:. a = v/t
-		// displacement = s = s0 + v0t + a*pow(t,2)/2
-		cout << abs(v*t*2) << endl;
+	while(scanf("%d %d",&v,&t)!=EOF){
+		printf("%d\n", v*t*2);
 	}
 
 	return 0;
