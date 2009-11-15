6329869d1582840f2a1ddd068e596c7ba40b241e
Lucas Simao <lsimaocosta@gmail.com>
2009-11-15 20:38:50 -0300

    Problema Hashmat the brave warrior concluida

--- Hashmat the brave warrior/Main.cpp
+++ Hashmat the brave warrior/Main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
-#include <string>
+#include <cmath>
 
 int main(int argc,char* argv[]){
+
+	int qtde1=0,qtde2=0;
+
+	while(scanf("%d %d",&qtde1,&qtde2)!=EOF){
+		printf("%d\n",abs(qtde1-qtde2));
+	}
 
 	return EXIT_SUCCESS;
 }
