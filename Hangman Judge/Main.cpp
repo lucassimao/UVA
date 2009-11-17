bf5762a7cc945ee92afceb570d5d3b78d1013399
Lucas Simao <lsimaocosta@gmail.com>
2009-11-17 09:35:39 -0300

    Mais uma tentativa no Hangman Judge

--- Hangman Judge/Main.cpp
+++ Hangman Judge/Main.cpp
@@ -2,55 +2,60 @@
 #include <string>
 using namespace std;
 
-#define MAX_ERRORS 7
-
 #define WIN_MSG "You win."
 #define LOSER_MSG "You lose."
 #define FAIL_MSG "You chickened out."
+#define MAX_ERROS 7
 
 int main(int argc,char **argv){
 
 	int round;
-	
-	while(true){
-		scanf("%d",&round);
-		cin.ignore();
 
-		if (round == -1) break;
-		else printf("Round %d\n", round);
+	while(true){
+
+		scanf("%d",&round);
+		if (round==-1) break;
+		else
+			printf("Round %d\n", round);
 
 		string palavra;
 		string guesses;
-		int erros = 0;
+
+		cin.ignore();
 
 		getline(cin,palavra);
 		getline(cin,guesses);
 
-		string::const_iterator iterator = guesses.begin();
+		string::const_iterator iterator =  guesses.begin();
+		int erros = 0;
 
-		while(iterator != guesses.end()){
+		while(palavra.length()>0 && iterator != guesses.end()){
 			char c = *iterator;
-			int pos = 0;
+			int pos = palavra.find(c);
 
-			while( palavra.length()>0 || erros> MAX_ERRORS ){
-				pos = palavra.find(c);
+			if (pos == -1) {erros++;++iterator;continue;}
+			else
+				do{
 
-				if (pos == -1){++erros;break;}
-				else{
-					if (pos==0)	palavra = palavra.substr(1);
-					else palavra = palavra.substr(0,pos) + palavra.substr(pos+1);
-				}
-			}
+				palavra = palavra.substr(0,pos) + palavra.substr(pos+1);
+
+			}while(palavra.length()>0 && (pos=palavra.find(c))!=-1);
+
 			++iterator;
+
 		}
-		if (erros > MAX_ERRORS){
-			printf("%s\n",LOSER_MSG);
-		}else
-		if (palavra.length() == 0){
+		if (palavra.length()==0)
 			printf("%s\n",WIN_MSG);
-		}
-		else printf("%s\n",FAIL_MSG);
-		
+		else
+			if (erros >= MAX_ERROS)
+				printf("%s\n",LOSER_MSG);
+			else
+				printf("%s\n",FAIL_MSG);
+
+
+
+
+
 	}
 	return 0;
 }
